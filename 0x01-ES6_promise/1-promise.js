919e1ec909ad387a5303f080da84989f5d33b7e5
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-05-29 17:57:16 -0700

    check task 2 it is very helpful to understand

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,15 +1,14 @@
 export default function getFullResponseFromAPI(success) {
-    const newPromise = new Promise((resolve, reject) => {
-        if(success){
-            const back = {
-                status: 200,
-                body: 'success'
-            }
-            resolve(back)
-        } else {
-            reject(new Error('The fake API is not working currently'))
-        }
-    })
-    return newPromise;
-
-}
+    return new Promise((resolve, reject) => {
+      if (success) {
+        resolve({
+          status: 200,
+          body: 'Success',
+        });
+      } else {
+        reject(
+          new Error('The fake API is not working currently'),
+        );
+      }
+    });
+  }
--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,12 @@
-export default function handleResponseFromAPI(promise){
-    promise.then((resolve)=>{
-        const obj = {
+export default function handleResponseFromAPI(promise) {
+    return promise.then((response) => {
+        console.log('Got a response from the API')
+        const object = {
             status: 200,
-            body: success
+            body: succuess
         }
-        console.log('zzzz') 
-    })
-
+        return object;
+    }).catch((error) => {
+       return 'fuccccccccccck'
+    });
 }
