f29343cc86b1493e962fd91d624e28b8a696f319
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-07-23 13:08:49 -0700

    check take input from stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // take input from user
 
-process.stdout.write("Welcome to Holberton School, what is your name?" + '\r\n')
+process.stdout.write("Welcome to Holberton School, what is your name?\n")
 process.stdin.on('data', data => {
     console.log(`Your name is: ${data}`)
 });
@@ -9,3 +9,6 @@ process.stdin.on('data', data => {
 process.stdin.on('end', () => {
     process.stdout.write('This important software is now closing\n');
   });
+
+
+
