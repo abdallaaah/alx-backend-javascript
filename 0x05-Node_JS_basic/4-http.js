fb4131eae74d13689e65ed2ac0fd7575504de9fb
abdallaaah <abdallah.hesham@msa.edu.eg>
2024-08-03 08:22:49 -0700

    check endpoint

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 // Create a small HTTP server using Node.js test
 
-const app = require('http');
+const http = require('http');
 
-app.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});   
   res.write('Hello Holberton School!');
   res.end();
 }).listen(1245);
 
-module.exports = app
+module.exports = app;
